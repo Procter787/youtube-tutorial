57fecc0d3b2f643b3be95200a27dbe04f1ed8629
Porter Morrison <port.morrison@gmail.com>
2021-11-17 13:49:15 -0700

    youtube-tutorial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
 import './App.css';
+import Projects from './components/projects'
+import React, { Component } from 'react';
+import AddProject from './components/AddProject';
+import {v4 as uuidv4} from 'uuid';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  constructor(){
+    super();
+    this.state = {
+      projects: []
+    }
+  }
+
+  getToDos(){
+
+  }
+
+  getProjects(){
+    this.setState({projects: [
+      {
+        id:uuidv4(),
+        title: "Buisness Website", 
+        category: "Web Design"
+      },  
+      {
+        id:uuidv4(),
+        title: "Social App", 
+        category: "Mobile Development"
+      },  
+      {
+        id:uuidv4(),
+        title: "Ecommerce Shopping Cart", 
+        category: "Web Development"
+      },  
+    ]});
+    this.getToDos();
+  }
+
+  componentWillMount(){
+    this.getProjects();
+    this.getToDos();
+  }
+
+  componentDidMount(){
+    this.getToDos();
+
+  }
+
+  handleAddProject(project){
+    let projects = this.state.projects;
+    projects.push(project);
+    this.setState({projects:projects});
+  }
+
+  handelDeleteProject(id){
+    let projects = this.state.projects;
+    let index = projects.findIndex(x => x.id === id);
+    projects.splice(index, 1);
+    this.setState({projects:projects});
+  }
+  
+  render(){
+return (
+  <div className="Projects">
+      <AddProject addProject={this.handleAddProject.bind(this)}/>
+      <Projects projects={this.state.projects} onDelete={this.handelDeleteProject.bind(this)} />
+  </div>
+);
+}
 }
 
 export default App;
